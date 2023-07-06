158692e24dffb23925ab1262d7906fc87f7a176f
bonex <bonexp@bonex>
2023-07-07 00:49:46 +0800

    base64 decode

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask,request
+import requests
+import base64
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/check_args', methods=['GET'])
+def check_args():
+    item_encoded = request.args.get('item')
+    key = request.args.get('key')
+
+    if item_encoded and key:
+        if key != 'abcabc':
+            return "Invalid key"
+        item_decoded = base64.b64decode(item_encoded).decode('utf-8')
+        response = requests.get(item_decoded)
+        
+        if response.status_code == 200:
+            content = response.text            
+            return f"{content}"
+        else:
+            return "Error: Unable to fetch content from the URL."  
+    if item_encoded and key:
+        return 'Both item and key are present in the arguments.'
+    else:
+        return 'Either item or key is missing in the arguments.'
